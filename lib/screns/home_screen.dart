ba0046af9b1f0a92f90b67c6228b853c496e6d9c
uetea69 <lucky.and.heaven@gmail.com>
2021-06-11 13:43:28 +0900

    エラー修正 body: 直下のExpandedをremove Stataクラスの綴り間違いを修正

--- lib/screns/home_screen.dart
+++ lib/screns/home_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScrState createState() => _HomeScrState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScrState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,56 +13,47 @@ class _HomeScrState extends State<HomeScreen> {
         title: Text("ツッコミマシーン"),
         centerTitle: true,
       ),
-      body: Expanded(
-        flex: 1,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                ],
-              ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+              ],
             ),
-            Expanded(
-              flex:1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                ],
-              ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                  Expanded(flex: 1,child: _soundButton()), //TODO ボタン
-                ],
-              ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+                Expanded(flex: 1, child: _soundButton()), //TODO ボタン
+              ],
             ),
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
   Widget _soundButton() {
     return Container(
-        chlid: RaisedButton(
-        ),
+      child: ElevatedButton(
+          child: Text(""), onPressed: () => print("sound_button is pushed")),
     );
   }
-
-
 }
-
-
-
